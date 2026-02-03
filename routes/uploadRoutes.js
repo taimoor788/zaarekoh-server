4bd6e462ddb5831e50160c184e94426ad8a462a3
Taimoor788 <taimoojan13967@gmail.com>
2026-02-04 03:09:23 +0500

    updation3

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -1,40 +1,15 @@
 const express = require('express');
 const multer = require('multer');
-const path = require('path');
 const router = express.Router();
+const { storage } = require('../config/cloudinary');
 
-const storage = multer.diskStorage({
-    destination(req, file, cb) {
-        cb(null, 'uploads/');
-    },
-    filename(req, file, cb) {
-        cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`);
-    },
-});
-
-function checkFileType(file, cb) {
-    const filetypes = /jpg|jpeg|png/;
-    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-    const mimetype = filetypes.test(file.mimetype);
-
-    if (extname && mimetype) {
-        return cb(null, true);
-    } else {
-        cb('Images only!');
-    }
-}
-
-const upload = multer({
-    storage,
-    fileFilter: function (req, file, cb) {
-        checkFileType(file, cb);
-    },
-});
+const upload = multer({ storage });
 
 router.post('/', upload.single('image'), (req, res) => {
-    res.send({
+    // Cloudinary returns the secure_url in req.file.path
+    res.json({
         message: 'Image uploaded',
-        filePath: `/uploads/${req.file.filename}`,
+        filePath: req.file.path
     });
 });
 
